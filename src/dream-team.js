177c8d78128f4f27d1bbdcd1c531f9831de4c1c6
elseElseIf <mihail.pryka@gmail.com>
2022-04-29 16:35:17 +0300

    feat:add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,7 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+  let result = [];
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] == "string"){
+      result.push(members[i].toUpperCase()[0]);
+    }
+  }
+  return result.sort().join("");
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
