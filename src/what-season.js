bc3aa9a3f55d66e47fa14d092f41f5de3491957f
elseElseIf <mihail.pryka@gmail.com>
2022-04-29 21:11:26 +0300

    feat:add what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result;
+  let springDate = date;
+  if(arguments.length == 0){
+    result = 'Unable to determine the time of year!';
+  } else if(springDate instanceof Date){
+  
+    let year = springDate.getYear();
+    let month = springDate.getMonth();
+    let day = springDate.getDay();
+    if(month == 11 || month == 0 || month == 1){
+      result = "winter";
+    }else if(month == 2 || month == 3 || month == 4){
+      result = "spring";
+    }else if(month == 5 || month == 6 || month == 7){
+      result = "summer";
+    }else if(month == 8 || month == 9 || month == 10){
+      result = "autumn";
+    }
+  } else{
+    result = 'Invalid date!';
+  };
+ return result;
+ 
 }
 
 module.exports = {
