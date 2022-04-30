31dca61a31f9502c4cbbeb964a5b5654d1b73bee
elseElseIf <mihail.pryka@gmail.com>
2022-04-30 21:09:26 +0300

    fix:add sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let elementArr = [];
+  let count = 0;
+  for(let el of arr){
+    if(el == -1){
+      elementArr.push(count);
+    }
+  count++;
+  }
+  arr.sort( (a, b) => a - b );
+
+  let result = arr.slice(elementArr.length);
+  for(let elem of elementArr){
+    result.splice(elem, 0, -1);
+  }
+  return result;
 }
 
 module.exports = {
